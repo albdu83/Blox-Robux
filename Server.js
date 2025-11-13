668510e7cbf26ee0ceaf1d18249876d401fba11d
louscript21 <louxi.clash21@gmail.com>
2025-11-13 19:59:19 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -70,7 +70,7 @@ if (formInscription) {
             lienprofil.textContent = `${connectedUser}`
             btnInscription.textContent = "Déconnexion";
             btnConnexion.textContent = "Commencer";
-            btnConnexion.href = "/Page de gain/gagner.html";
+            btnConnexion.href = "./Page de gain/gagner.html";
             btnInscription.removeAttribute('href')
             btnInscription.style.cursor = "pointer";
             // Actions
@@ -147,6 +147,7 @@ document.addEventListener("DOMContentLoaded", () => {
     togglePassword("showPassword2", "password");          // formulaire inscription
     togglePassword("showPassword3", "confirmPassword");   // confirmation mot de passe
 });
+
 
 
 
