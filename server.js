7e9868488665aa40607a8bd956f8147b846426ac
louscript21 <louxi.clash21@gmail.com>
2025-12-06 19:11:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-//ok
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend Render OK ✔️");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
