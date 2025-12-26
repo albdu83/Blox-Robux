22e129038dbd1f2d385908a7d97b34b68e3a13c8
louscript21 <louxi.clash21@gmail.com>
2025-12-26 01:56:11 +0100

    Update request.js

--- request.js
+++ request.js
@@ -159,7 +159,7 @@ app.get("/reach", async (req, res) => {
     const fullUrl =
       req.protocol + "://" + req.get("host") + req.originalUrl;
 
-    const urlWithoutHash = fullUrl.split("&hash=")[0];
+    const urlWithoutHash = 'http://blox-robux.onrender.com/reach?user_id=Alban&app_id=24471&reward=100&status=1&currency=1.67&screenout=2&profiler=2&tx_id=TEST123&ip=127.0.0.1&offer_id=campaign_this_is_a_test_campaign_id&debug=true';
 
     const computedHash = crypto
       .createHmac("sha1", THEOREM_SECRET) // 🔥 HMAC SHA1
