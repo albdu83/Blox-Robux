5b0dc772c3422b23365d47baef0c0e71527a6888
louscript21 <louxi.clash21@gmail.com>
2026-04-27 01:12:05 +0200

    Update request.js

--- request.js
+++ request.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const crypto = require("crypto");
-const fetch = require("node-fetch"); // si Node < 18
 const app = express();
 const helmet = require("helmet");
 const path = require("path");
