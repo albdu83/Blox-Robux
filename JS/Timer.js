cd2cd0d07f4e8e52025978d7830c007ad43e6b01
louscript21 <louxi.clash21@gmail.com>
2026-06-22 11:35:31 +0200

    Update Timer.js

--- JS/Timer.js
+++ JS/Timer.js
@@ -1,12 +1,4 @@
-async function getEventEndFromServer() {
-  const res = await fetch("https://il.bloxrbx.fr/api/event/info");
-  return await res.json();
-}
-
 document.addEventListener("DOMContentLoaded", async () => {
-  const data = await getEventEndFromServer();
-  const eventStart = data.eventStart * 1000;
-
   const events = document.querySelectorAll("[data-event]");
   const overlay = document.querySelectorAll("#overlay")[0];
   const closeModal = document.querySelectorAll("#closeModal")[0];
@@ -20,48 +12,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   events.forEach((card) => {
-    const joinBtn = card.querySelectorAll("[data-join]")[0];
-    const hoursEl = card.querySelectorAll("[data-hours]")[0];
-    const minutesEl = card.querySelectorAll("[data-minutes]")[0];
-    const secondsEl = card.querySelectorAll("[data-seconds]")[0];
-    const detailsBtn = card.querySelectorAll("[data-details]")[0];
+    const detailsBtn = card.querySelector("[data-details]");
+    const joinBtn = card.querySelector(".join-btn");
 
-    let timer;
-
-    joinBtn.disabled = true;
-    joinBtn.style.background = "#735b18";
-
-    const updateTimer = () => {
-      const remainingTime = Math.floor((eventStart - Date.now()) / 1000);
-
-      if (remainingTime <= 0) {
-        clearInterval(timer);
-
-        hoursEl.textContent = "00";
-        minutesEl.textContent = "00";
-        secondsEl.textContent = "00";
-
-        joinBtn.disabled = false;
-        joinBtn.style.background = "#f0c240";
-
-        joinBtn.onclick = () => {
-          window.location.href = data.eventUrl;
-        };
-
-        return;
-      }
-
-      const h = Math.floor(remainingTime / 3600);
-      const m = Math.floor((remainingTime % 3600) / 60);
-      const s = remainingTime % 60;
-
-      hoursEl.textContent = String(h).padStart(2, "0");
-      minutesEl.textContent = String(m).padStart(2, "0");
-      secondsEl.textContent = String(s).padStart(2, "0");
+    joinBtn.onclick = () => {
+      navigate("/Pages/Event");
     };
-
-    updateTimer();
-    timer = setInterval(updateTimer, 1000);
 
     if (detailsBtn) {
       detailsBtn.addEventListener("click", () => {
